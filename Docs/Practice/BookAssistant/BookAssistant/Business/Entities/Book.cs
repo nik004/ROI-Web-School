b7159c4b94b27cf22ee1759c4422e3c719128ba8
olga-torkhanova <Olga.Torkhanova@returnonintelligence.com>
2014-12-11 11:48:27 +0300

    added Book assistant project

---
+++ Docs/Practice/BookAssistant/BookAssistant/Business/Entities/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookAssistant.Bussines.Entities
+{
+    public class Book
+    {
+        public int Id { set; get; }
+        public string Title { set; get; }
+        public string Description { set; get; }
+        public string Author { set; get; }
+        public string Img { set; get; }
+    }
+}
---
+++ Docs/Practice/BookAssistant/BookAssistant/Models/BooksViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using BookAssistant.Bussines.Entities;
+
+namespace BookAssistant.Models
+{
+    public class BooksViewModel
+    {
+        public List<Book> Items { set; get; }
+        public Book SelectedBook { set; get; }
+    }
+}
