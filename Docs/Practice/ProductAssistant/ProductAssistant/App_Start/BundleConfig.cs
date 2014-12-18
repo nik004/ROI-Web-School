61cdf0ec89e82a3e6411ac09a6b56ff8eb50d8bd
olga-torkhanova <Olga.Torkhanova@returnonintelligence.com>
2014-12-18 13:28:11 +0300

    Added Web UI part for product assistant

--- Docs/Practice/ProductAssistant/ProductAssistant/App_Start/BundleConfig.cs
+++ Docs/Practice/ProductAssistant/ProductAssistant/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace ProductAssistant
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/product").Include(
+                        "~/Scripts/ProductAssistant.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
 
---
+++ Docs/Practice/ProductAssistant/ProductAssistant/Controllers/ProductAssistantController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductAssistant.Controllers
+{
+    public class ProductAssistantController : Controller
+    {
+        //
+        // GET: /ProductAssistant/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
